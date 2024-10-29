39258b2d57223ee01ec3812b00c32f6ac875b4ae
SANDHRI JOHN ABHISHEK <asandhrijohn@gmail.com>
2024-10-29 18:21:50 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,27 @@
+const btn=document.getElementsByClassName('btn')[0];
+
+const modal=document.getElementsByClassName('modal')[0];
+
+const cross=document.getElementsByClassName('cross')[0];
+
+const body=document.body;
+
+
+btn.addEventListener('click', function(){
+    console.log("clicked");
+    modal.classList.remove('hide');
+    body.classList.add('overlay');
+})
+
+cross.addEventListener('click', function(){
+    modal.classList.add('hide');
+    body.classList.remove('overlay');
+})
+
+document.addEventListener('keydown', function(e){
+    if(e.keyCode==27){
+        modal.classList.add('hide');
+        body.classList.remove('overlay');
+
+    }
+})
